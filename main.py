7d31228261aa00577d980e5f7e7dfc90b891c328
InShuo <2985469325@qq.com>
2026-09-21 21:30:24 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#print("THIS IS A TEST MASSAGE")
+A=int(input("请键入一个数字："))
+B=int(input("请键入一个另一个你想乘的数字："))
+C=int(A*B)
+print(C)
+
+D=int(input("请键入一个数字："))
+E=int(input("请键入一个另一个你想乘的数字："))
+print("输出结果为",D*E)
+
+A=int(input("请键入一个数字："))
+B=int(input("请键入一个另一个你想乘的数字："))
+C=(A*B)
+print(C)
+
+A=(input("请键入一个数字："))
+B=(input("请键入一个另一个你想乘的数字："))
+C=(A*B)
+print(C)
