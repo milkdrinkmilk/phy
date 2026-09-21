cb5fc7545543e096c35737e10724c086a0fcb54e
InShuo <2985469325@qq.com>
2026-09-21 22:08:54 +0800

    new version

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 #print("THIS IS A TEST MASSAGE")
-A=int(input("请键入一个数字："))
-B=int(input("请键入一个另一个你想乘的数字："))
-C=int(A*B)
-print(C)
+A=int(input("请键入一个数字："))    #A变量目前是整数形式
+B=int(input("请键入一个另一个你想乘的数字："))  #B量目前是整数形式
+C=(A*B)     #C变量目前是整数形式
+print(C)    
+D=str(5)    #D变量目前是str字符串
+D=str(D)
+C=str(C)    #C变量目前是str字符串
+print(C+D)
+#print(C*D)
 
-D=int(input("请键入一个数字："))
-E=int(input("请键入一个另一个你想乘的数字："))
-print("输出结果为",D*E)
 
-A=int(input("请键入一个数字："))
-B=int(input("请键入一个另一个你想乘的数字："))
-C=(A*B)
-print(C)
-
-A=(input("请键入一个数字："))
-B=(input("请键入一个另一个你想乘的数字："))
-C=(A*B)
-print(C)
+#input默认输入的是字符串
+#str是字符串
+#int是整数
+#float是浮点数
